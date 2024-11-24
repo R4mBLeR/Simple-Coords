33d497486f8fa968502281f4c81b094dfa514f5e
R4mBLe_ <misharad2008@gmail.com>
2024-11-25 00:10:41 +0300

    v1.0.1

--- src/client/java/net/r4mble/simplecoords/SimpleCoordsClient.java
+++ src/client/java/net/r4mble/simplecoords/SimpleCoordsClient.java
@@ -1,10 +1,12 @@
 package net.r4mble.simplecoords;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.r4mble.simplecoords.client.gui.CoordsHud;
 
 public class SimpleCoordsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		HudRenderCallback.EVENT.register(new CoordsHud());
 	}
 }
