e476ea41b71a46f9e8ce82eded1a3db31d7d7ca6
R4mBLe_ <misharad2008@gmail.com>
2024-11-29 16:43:54 +0300

    add Notice Screen

--- src/client/java/net/r4mble/simplecoords/ModMenuIntegration.java
+++ src/client/java/net/r4mble/simplecoords/ModMenuIntegration.java
@@ -6,6 +6,9 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.NoticeScreen;
+import net.minecraft.text.Text;
 import net.r4mble.simplecoords.config.ModConfigData;
 
 @Environment(EnvType.CLIENT)
@@ -17,7 +20,7 @@ public class ModMenuIntegration implements ModMenuApi {
             if(FabricLoader.getInstance().isModLoaded("cloth-config")) {
                 return AutoConfig.getConfigScreen(ModConfigData.class, parent).get();
             }
-            return parent;
+            return new NoticeScreen(() -> MinecraftClient.getInstance().setScreen(parent), Text.of("Simple Coords"), Text.of("Simple Coords requires Cloth Config to configure the mod, adding new values to the HUD"));
         };
     }
 }
