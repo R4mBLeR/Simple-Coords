f9e429d4e0e437d05c178b0d4efe6deea8dc646f
R4mBLe_ <misharad2008@gmail.com>
2024-12-07 15:22:05 +0300

    add changble hud scale

--- src/client/java/net/r4mble/simplecoords/gui/CoordsHud.java
+++ src/client/java/net/r4mble/simplecoords/gui/CoordsHud.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.r4mble.simplecoords.SimpleCoordsClient;
 
@@ -27,39 +28,45 @@ public class CoordsHud implements HudRenderCallback {
             String facing;
             String fps;
             String biome;
-            if (SimpleCoordsClient.config.showCoords()) {
-                double x = player.getX();
-                double y = player.getY();
-                double z = player.getZ();
-                coords = String.format("X: %.2f Y: %.2f Z: %.2f", x, y, z).replace(',', '.');
-                textWidth = Math.max(textWidth, client.textRenderer.getWidth(coords));
-                drawContext.drawTextWithShadow(client.textRenderer, coords, posX, posY, 0xFFFFFF);
-                drawCount++;
+            if (SimpleCoordsClient.config.HudScale() > 0f) {
+                MatrixStack matrices = drawContext.getMatrices();
+                matrices.push();
+                matrices.scale(SimpleCoordsClient.config.HudScale(), SimpleCoordsClient.config.HudScale(), 1);
+                if (SimpleCoordsClient.config.showCoords()) {
+                    double x = player.getX();
+                    double y = player.getY();
+                    double z = player.getZ();
+                    coords = String.format("X: %.2f Y: %.2f Z: %.2f", x, y, z).replace(',', '.');
+                    textWidth = Math.max(textWidth, client.textRenderer.getWidth(coords));
+                    drawContext.drawTextWithShadow(client.textRenderer, coords, posX, posY, 0xFFFFFF);
+                    drawCount++;
 
-            }
-            if (SimpleCoordsClient.config.showFacing()) {
-                facing = player.getHorizontalFacing().toString();
-                facing = "Facing: " + facing.substring(0, 1).toUpperCase() + facing.substring(1);
-                textWidth = Math.max(textWidth, client.textRenderer.getWidth(facing));
-                drawContext.drawTextWithShadow(client.textRenderer, facing, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
-                drawCount++;
+                }
+                if (SimpleCoordsClient.config.showFacing()) {
+                    facing = player.getHorizontalFacing().toString();
+                    facing = "Facing: " + facing.substring(0, 1).toUpperCase() + facing.substring(1);
+                    textWidth = Math.max(textWidth, client.textRenderer.getWidth(facing));
+                    drawContext.drawTextWithShadow(client.textRenderer, facing, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
+                    drawCount++;
 
-            }
-            if (SimpleCoordsClient.config.showBiome()) {
-                biome = "Biome: " + player.getWorld().getBiome(player.getBlockPos()).getKey().get().getValue().toString();
-                textWidth = Math.max(textWidth, client.textRenderer.getWidth(biome));
-                drawContext.drawTextWithShadow(client.textRenderer, biome, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
-                drawCount++;
-            }
-            if (SimpleCoordsClient.config.showFPS()) {
-                fps = client.getCurrentFps() + " fps";
-                textWidth = Math.max(textWidth, client.textRenderer.getWidth(fps));
-                drawContext.drawTextWithShadow(client.textRenderer, fps, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
-                drawCount++;
+                }
+                if (SimpleCoordsClient.config.showBiome()) {
+                    biome = "Biome: " + player.getWorld().getBiome(player.getBlockPos()).getKey().get().getValue().toString();
+                    textWidth = Math.max(textWidth, client.textRenderer.getWidth(biome));
+                    drawContext.drawTextWithShadow(client.textRenderer, biome, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
+                    drawCount++;
+                }
+                if (SimpleCoordsClient.config.showFPS()) {
+                    fps = client.getCurrentFps() + " fps";
+                    textWidth = Math.max(textWidth, client.textRenderer.getWidth(fps));
+                    drawContext.drawTextWithShadow(client.textRenderer, fps, posX, posY + drawCount * textHeight + 1, 0xFFFFFF);
+                    drawCount++;
 
-            }
-            if (drawCount > 0) {
-                drawContext.fill(0, 0, textWidth + padding + offset, posY + drawCount * textHeight + padding, 0x30000000);
+                }
+                if (drawCount > 0) {
+                    drawContext.fill(0, 0, textWidth + padding + offset, posY + drawCount * textHeight + padding, 0x30000000);
+                }
+                matrices.pop();
             }
         }
     }
