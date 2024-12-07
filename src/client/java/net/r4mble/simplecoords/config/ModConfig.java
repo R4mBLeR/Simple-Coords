de900ae2ebedaf79841adc97461173a6b6c51d61
R4mBLe_ <misharad2008@gmail.com>
2024-12-07 15:22:05 +0300

    add changble hud scale

--- src/client/java/net/r4mble/simplecoords/config/ModConfig.java
+++ src/client/java/net/r4mble/simplecoords/config/ModConfig.java
@@ -1,8 +1,13 @@
 package net.r4mble.simplecoords.config;
 
 public interface ModConfig {
+    float HudScale();
+
     boolean showCoords();
+
     boolean showFacing();
+
     boolean showBiome();
+
     boolean showFPS();
 }
